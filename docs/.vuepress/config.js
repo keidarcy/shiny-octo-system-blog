8f2bf332c7f423a6519958eb5c897da8ab8dc746
XING YAHAO <keidarcy.1015@gmail.com>
2020-03-31 10:07:20 +0900

    content

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,33 +20,7 @@ module.exports = {
       'meta',
       { property: 'og:image', content: '/icons/apple-icon-152x152.png' }
     ],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    [
-      'meta',
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
-    ],
-    [
-      'link',
-      { rel: 'apple-touch-icon', href: '/icons/apple-icon-152x152.png' }
-    ],
-    [
-      'link',
-      {
-        rel: 'mask-icon',
-        href: '/icons/apple-icon-152x152.png',
-        color: '#3eaf7c'
-      }
-    ],
-    [
-      'meta',
-      {
-        name: 'msapplication-TileImage',
-        content: '/icons/apple-icon-144x144.png'
-      }
-    ],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['link', { rel: 'manifest', href: '/manifest.json' }]
   ],
   theme: 'reco',
   plugins: [
